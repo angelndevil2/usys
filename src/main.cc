8e1aec983e2c959643055353b9276bf79d209105
k <angelndevil2@gmail.com>
2019-01-31 04:01:26 +0900

    separate main

---
+++ src/main.cc
@@ -0,0 +1,29 @@
+#include <proc_stat.h>
+#include <invalid_string_exception.h>
+
+using namespace usys;
+
+int main()
+{
+
+    try
+    {
+        ProcStat ps = ProcStat();
+        ps.init();
+
+        std::cout << "kernel clock tick per second: " << ps.clock_tick() << std::endl;
+
+        sleep(2);
+
+        ps.set_current_cpu_usage();
+
+        std::cout << ps.cpu_sysem(0) << std::endl;
+        printf("%.2f\n", ps.cpu_idle(0));
+
+        return 0;
+    }
+    catch (InvalidString &is)
+    {
+        std::cout << is.what() << std::endl;
+    }
+}
--- src/proc_stat.cc
+++ src/proc_stat.cc
@@ -199,25 +199,3 @@ float ProcStat::cpu_guest_nice(int ncpu)
 } // end of namespace
 
 using namespace usys;
-int main() {
-
-    try {
-        ProcStat ps = ProcStat();
-        ps.init();
-
-
-        std::cout <<"kernel clock tick per second: " << ps.clock_tick() << std::endl;
-
-        sleep(2);
-
-        ps.set_current_cpu_usage();
-
-        std::cout << ps.cpu_sysem(0) << std::endl;
-        printf("%.2f\n", ps.cpu_idle(0));
-
-        return 0;
-    }
-    catch (InvalidString& is) {
-        std::cout << is.what() << std::endl;
-    }
-}
